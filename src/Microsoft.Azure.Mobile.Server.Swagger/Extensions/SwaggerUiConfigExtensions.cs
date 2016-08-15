0a4f801a142b37570c3cdcce730783e4a947ed16
Brett Samblanet <brettsam@microsoft.com>
2016-08-15 14:31:17 -0700

    moving to newest swashbuckle

--- src/Microsoft.Azure.Mobile.Server.Swagger/Extensions/SwaggerUiConfigExtensions.cs
+++ src/Microsoft.Azure.Mobile.Server.Swagger/Extensions/SwaggerUiConfigExtensions.cs
@@ -3,6 +3,7 @@
 // ----------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Web.Http;
 using Microsoft.Azure.Mobile.Server;
@@ -23,12 +24,13 @@ namespace Swashbuckle.Application
             {
                 throw new ArgumentNullException("httpConfig");
             }
-            
+
             httpConfig.MessageHandlers.Add(new SwaggerUiSecurityFilter(httpConfig));
 
             Assembly thisAssembly = typeof(SwaggerUiConfigExtensions).Assembly;
             config.CustomAsset("o2c-html", thisAssembly, "Microsoft.Azure.Mobile.Server.Swagger.o2c.html");
-            config.CustomAsset("lib/swagger-oauth-js", thisAssembly, "Microsoft.Azure.Mobile.Server.Swagger.swagger-oauth.js");
+            config.CustomAsset("swagger-ui-min-js", thisAssembly, "Microsoft.Azure.Mobile.Server.Swagger.swagger-ui.min.js");
+            config.CustomAsset("index", thisAssembly, "Microsoft.Azure.Mobile.Server.Swagger.index.html");
 
             return config;
         }
