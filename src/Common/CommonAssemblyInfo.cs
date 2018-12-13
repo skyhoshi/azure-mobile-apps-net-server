775643c9ca6dbe1f35697e4e972a89c08e3996aa
Guillaume Perrot <guperrot@microsoft.com>
2018-12-13 14:11:06 -0800

    Allow latest versions of JWT for Authentication package (but not Login)

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Runtime.InteropServices;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: AssemblyMetadata("Serviceable", "True")]
 
-[assembly: AssemblyVersion("2.0.3")]
-[assembly: AssemblyFileVersion("2.0.3")]
-[assembly: AssemblyInformationalVersion("2.0.3")]
+[assembly: AssemblyVersion("2.0.4")]
+[assembly: AssemblyFileVersion("2.0.4")]
+[assembly: AssemblyInformationalVersion("2.0.4")]
